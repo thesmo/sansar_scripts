7cc93e8bda66a3ee3c2ea68be42c90585b42f65a
thesmo <cosmo@eveny.net>
2018-05-25 15:06:01 -0700

    add smojoin.cs for using when people join the experience

---
+++ smojoin.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sansar;
+using Sansar.Script;
+using Sansar.Simulation;
+
+//Some examples taken from https://help.sansar.com/hc/en-us/articles/115003373967-Sansar-Script-API
+
+public class SmoJoin : SceneObjectScript
+{
+    public override void Init() {
+        Script.UnhandledException += UnhandledException;
+        // Subscribe to Add User events. Use SessionId.Invalid to track all users.
+        ScenePrivate.User.Subscribe(User.AddUser, SessionId.Invalid, AddUser);
+    }
+
+    void AddUser(string Action, Sansar.Script.SessionId User, string Data) {
+        // Lookup the name of the agent.
+        string name = ScenePrivate.FindAgent(User).AgentInfo.Name;
+        ScenePrivate.Chat.MessageAllUsers(string.Format("{0} Has Entered the Smotel!", name));
+        AgentPrivate agent = ScenePrivate.FindAgent(User);
+        agent.SendChat($"type !commands for a list");
+    }
+    
+    private void UnhandledException(object sender, Exception e) {
+        if(!Script.UnhandledExceptionRecoverable)
+        {
+            Log.Write(LogLevel.Error, GetType().Name, $"Error: {e.Message}\n\n{e.StackTrace}");
+        } else
+        {
+            Log.Write(LogLevel.Warning, GetType().Name, $"Warning: {e.Message}\n\n{e.StackTrace}");
+        }
+    }
+}
